1536e7f649d1b4fb54fb86633ca0c32c359d0336
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2021-09-17 16:36:10 +0700

    Added explicit `unsigned`

--- Include/cpython/bytesobject.h
+++ Include/cpython/bytesobject.h
@@ -5,7 +5,7 @@
 typedef struct {
     PyObject_VAR_HEAD
     Py_hash_t ob_shash;
-    char ob_sval[1];
+    unsigned char ob_sval[1];
 
     /* Invariants:
      *     ob_sval contains space for 'ob_size+1' elements.
