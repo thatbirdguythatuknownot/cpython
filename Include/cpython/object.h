c07a3cc8e33689b58ece3a564930a3a7faa59424
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2021-11-02 14:06:04 +0800

    Fixed missing slots error

--- Include/cpython/object.h
+++ Include/cpython/object.h
@@ -146,6 +146,8 @@ typedef struct {
 
     binaryfunc nb_matrix_multiply;
     binaryfunc nb_inplace_matrix_multiply;
+    unaryfunc nb_increment;
+    unaryfunc nb_decrement;
 } PyNumberMethods;
 
 typedef struct {
