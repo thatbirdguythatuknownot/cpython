60e06767a963b56d552f7bdfd8f71eae3cf32613
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-03-02 08:24:04 +0800

    Update operator.py

--- Lib/operator.py
+++ Lib/operator.py
@@ -11,9 +11,9 @@ This is the pure Python implementation of the module.
 """
 
 __all__ = ['abs', 'add', 'and_', 'attrgetter', 'call', 'concat', 'contains', 'countOf',
-           'delitem', 'eq', 'floordiv', 'ge', 'getitem', 'gt', 'iadd', 'iand',
+           'decrement', 'delitem', 'eq', 'floordiv', 'ge', 'getitem', 'gt', 'iadd', 'iand',
            'iconcat', 'ifloordiv', 'ilshift', 'imatmul', 'imod', 'imul',
-           'index', 'indexOf', 'inv', 'invert', 'ior', 'ipow', 'irshift',
+           'increment', 'index', 'indexOf', 'inv', 'invert', 'ior', 'ipow', 'irshift',
            'is_', 'is_not', 'isub', 'itemgetter', 'itruediv', 'ixor', 'le',
            'length_hint', 'lshift', 'lt', 'matmul', 'methodcaller', 'mod',
            'mul', 'ne', 'neg', 'not_', 'or_', 'pos', 'pow', 'rshift',
@@ -80,9 +80,17 @@ def and_(a, b):
     "Same as a & b."
     return a & b
 
+def decrement(a):
+    "Same as --a."
+    return --a
+
 def floordiv(a, b):
     "Same as a // b."
     return a // b
+
+def increment(a):
+    "Same as ++a."
+    return ++a
 
 def index(a):
     "Same as a.__index__()."
@@ -430,7 +438,9 @@ __abs__ = abs
 __add__ = add
 __and__ = and_
 __call__ = call
+__dec__ = decrement
 __floordiv__ = floordiv
+__inc__ = increment
 __index__ = index
 __inv__ = inv
 __invert__ = invert
