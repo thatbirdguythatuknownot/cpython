6028a788416f803dd534bc4b1be8e062587a3b9e
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-03-15 14:30:46 +0800

    Convert malloc + memset into calloc

--- Include/internal/pycore_pyarena.h
+++ Include/internal/pycore_pyarena.h
@@ -52,6 +52,8 @@ PyAPI_FUNC(void) _PyArena_Free(PyArena *);
  */
 PyAPI_FUNC(void*) _PyArena_Malloc(PyArena *, size_t size);
 
+PyAPI_FUNC(void*) _PyArena_Calloc(PyArena *, size_t nitems, size_t elsize);
+
 /* This routine isn't a proper arena allocation routine.  It takes
  * a PyObject* and records it so that it can be DECREFed when the
  * arena is freed.
