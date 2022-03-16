9d48df01abba7426e4bf04db85b88d45825f83a2
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-03-16 14:57:39 +0800

    Convert malloc + memset into calloc

--- Python/pyarena.c
+++ Python/pyarena.c
@@ -119,7 +119,7 @@ block_alloc(block *b, size_t size)
 {
     void *p;
     assert(b);
-    size_t size = _Py_SIZE_ROUND_UP(size, ALIGNMENT);
+    size = _Py_SIZE_ROUND_UP(size, ALIGNMENT);
     if (b->ab_offset + size > b->ab_size) {
         /* If we need to allocate more memory than will fit in
            the default block, allocate a one-off block that is
@@ -146,7 +146,7 @@ block_calloc(block *b, size_t nitems, size_t elsize)
 {
     void *p;
     assert(b);
-    size = _Py_SIZE_ROUND_UP(nitems * elsize, ALIGNMENT);
+    size_t size = _Py_SIZE_ROUND_UP(nitems * elsize, ALIGNMENT);
     if (b->ab_offset + size > b->ab_size) {
         /* If we need to allocate more memory than will fit in
            the default block, allocate a one-off block that is
